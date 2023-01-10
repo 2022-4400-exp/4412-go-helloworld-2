b4ccd791eefafc010369a405b338e298e100636a
OmoriShu <teryy11131012@gmail.com>
2023-01-10 13:37:29 +0900

    changed message

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.String(http.StatusOK, "4412:vamo ver.2")
+        return c.String(http.StatusOK, "4412:paa ver.2")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4412", "message": "vamo"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4412", "message": "paa"})
     }
 }
